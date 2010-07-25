1b15bfa9e55ddfe0a55c35c880cc41a5b930c05e
zephyr.owl <zephyr.owl@5eb10746-d8a1-70fc-444b-4eb12e85a3b0>
2010-07-25 11:09:43 +0000

    Added throws and some other stuff.

--- MeleeTools/MeleeLib/ScriptCommand.cs
+++ MeleeTools/MeleeLib/ScriptCommand.cs
@@ -11,10 +11,20 @@ namespace MeleeLib
         public static ScriptCommand Factory(byte* ptr)
         {
             var gc = new GenericCommand(ptr);
-            if (gc.Type == 0x1 || gc.Type == 0x2)
-                return new TimerCommand(ptr);
-            if (gc.Type == 0xb)
-                return new HitboxCommand(ptr);
+            switch (gc.Type)
+            {
+                case 0x01:
+                case 0x02:
+                    return new TimerCommand(ptr);
+                case 0x03:
+                    return new StartLoopCommand(ptr);
+                case 0x07:
+                    return new PointerCommand(ptr);
+                case 0x0b:
+                    return new HitboxCommand(ptr);
+                case 0x88:
+                    return new ThrowCommand(ptr);
+            }
             return new GenericCommand(ptr);
         }
         struct CommandData
@@ -49,20 +59,23 @@ namespace MeleeLib
             dict[0x11] = new CommandData(0x0c, "Sound Effect");
             dict[0x12] = new CommandData(0x0c, "Random Smash SFX");
             dict[0x13] = new CommandData(0x04, "Autocancel");
+            dict[0x14] = new CommandData(0x04, "Reverse Direction");
 
             dict[0x17] = new CommandData(0x04, "IASA");
 
+            dict[0x19] = new CommandData(0x04, "Visibility?");
             dict[0x1a] = new CommandData(0x04, "Body Invincible");
             dict[0x1b] = new CommandData(0x04, "Body Invincible2");
             dict[0x1c] = new CommandData(0x04, "Partial Invincible");
 
             dict[0x1f] = new CommandData(0x04, "Model Mod");
-
             dict[0x33] = new CommandData(0x04, "Self-Damage");
 
             dict[0x36] = new CommandData(0x12, "Unknown");
             dict[0x37] = new CommandData(0x0c, "Unknown");
             dict[0x38] = new CommandData(0x08, "Start Smash Charge");
+
+            dict[0x88] = new CommandData(0x88, "Throw");
         }
         static int getLength(uint type)
         {
@@ -89,7 +102,7 @@ namespace MeleeLib
         {
             get
             {
-                StringBuilder sb = new StringBuilder();
+                var sb = new StringBuilder();
                 for (int i = 0; i < Length; i++)
                 {
                     sb.AppendFormat("{0:X2} ", data[i]);
@@ -99,11 +112,17 @@ namespace MeleeLib
         }
         protected byte* data;
     }
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ParamAttribute : System.Attribute
+    {
+    }
+
     public unsafe class GenericCommand : ScriptCommand
     {
-        public GenericCommand(byte* ptr) : base(ptr)
+        public GenericCommand(byte* ptr)
+            : base(ptr)
         {
-           
+
         }
     }
     public unsafe class TimerCommand : ScriptCommand
@@ -111,55 +130,28 @@ namespace MeleeLib
         public TimerCommand(byte* ptr)
             : base(ptr)
         {
-           
+
         }
-        public string Name
+        public new string Name
         {
             get { return base.Name + "[" + Frames + "]"; }
         }
         [CategoryAttribute("Timer Params")]
         public ushort Frames
         {
-            get{return *(bushort*)(data+2);}
+            get { return *(bushort*)(data + 2); }
         }
     }
-    public unsafe class HitboxCommand : ScriptCommand
+    public unsafe class HitboxCommand : CollisionCommand
     {
         public HitboxCommand(byte* ptr)
             : base(ptr)
         {
 
         }
-        public enum HIFlags
+        public enum HurtboxInteractionFlags
         {
-            NO_CLANK,SOME_CLANK,MORE_CLANK,ALL_CLANK
-        }
-        public enum HBIFlags
-        {
-            NONE,AIR_ONLY,GROUND_ONLY,NORMAL
-        }
-        public enum ElementType
-        {
-            NORMAL = 0x00,
-            FIRE = 0x04,
-            ELECTRIC = 0x08,
-            SLASH= 0x0C,
-            COIN= 0x10,
-            ICE = 0x14,
-            SLEEP=0x18,
-            SLEEP2=0x1C,
-            GROUNDED=0x20,
-            GROUNDED2=0x24,
-            CAPE = 0x28,
-            EMPTY=0x2C,
-            DISABLED = 0x30,
-            SCREW_ATTACK = 0x38,
-            POISON_FLOWER=0x3C,
-            NOTHING = 0x40
-        }
-        new public string Name
-        {
-            get { return base.Name + "[" + ID + "]"; }
+            NoClank, SomeClank, MoreClank, AllClank
         }
         public int ID
         {
@@ -172,16 +164,11 @@ namespace MeleeLib
         [CategoryAttribute("Stats")]
         public int BoneID
         {
-            get { return *(bushort*)(data+1) >> 3 & 0x7F; }
+            get { return *(bushort*)(data + 1) >> 3 & 0x7F; }
         }
         public int Unknown0
         {
             get { return ((data[2] & 0x07) >> 1); }
-        }
-        [CategoryAttribute("Stats")]
-        public int Damage
-        {
-            get { return *(bushort*)(data + 2) >> 0 & 0x1FF; }
         }
         [CategoryAttribute("Position")]
         public int Size
@@ -203,39 +190,19 @@ namespace MeleeLib
         {
             get { return *(bshort*)(data + 10); }
         }
-        [CategoryAttribute("Stats")]
-        public int Angle
-        {
-            get { return *(bushort*)(data + 12) >> 7 & 0xFFFF; }
-        }
-        [CategoryAttribute("Stats")]
-        public int KBGrowth
-        {
-            get { return *(bushort*)(data + 13) >> 6 & 0x1FF; }
-        }
-        [CategoryAttribute("Stats")]
-        public int WeightDependantKB
-        {
-            get { return *(bushort*)(data + 14) >> 5 & 0x1FF; }
-        }
         public int UnknownQ
         {
             get { return (data[15]) >> 2 & 0x7; }
         }
         [CategoryAttribute("Flags")]
-        public HIFlags HitboxInteraction
+        public HurtboxInteractionFlags HurtboxInteraction
         {
-            get { return (HIFlags)((data[15]) >> 0 & 0x3); }
+            get { return (HurtboxInteractionFlags)((data[15]) >> 0 & 0x3); }
         }
         [CategoryAttribute("Stats")]
-        public int BaseKB
+        public int BaseKnockback
         {
             get { return *(bushort*)(data + 16) >> 7 & 0xFFFF; }
-        }
-        [CategoryAttribute("Stats")]
-        public ElementType Element
-        {
-            get { return (ElementType)(*(bushort*)(data + 17) >> 2 & 0x1F); }
         }
         public int UnknownV
         {
@@ -252,9 +219,132 @@ namespace MeleeLib
             get { return *(bushort*)(data + 18) >> 2 & 0xFF; }
         }
         [CategoryAttribute("Flags")]
-        public HBIFlags HurtBoxInteraction
+        public bool HitsGround
         {
-            get { return (HBIFlags)((data[19]) >> 0 & 0x3); }
+            get { return Convert.ToBoolean((data[19]) >> 1 & 0x1); }
+        }
+        [CategoryAttribute("Flags")]
+        public bool HitsAir
+        {
+            get { return Convert.ToBoolean((data[19]) >> 0 & 0x1); }
+        }
+        public new string Name
+        {
+            get { return base.Name + "[" + ID + "]"; }
+        }
+    }
+    public unsafe abstract class CollisionCommand : ScriptCommand
+    {
+        protected CollisionCommand(byte* dataptr)
+            : base(dataptr)
+        {
+        }
+
+
+        public enum ElementType
+        {
+            Normal = 0x00,
+            Fire = 0x04,
+            Electric = 0x08,
+            Slash = 0x0C,
+            Coin = 0x10,
+            Ice = 0x14,
+            Sleep = 0x18,
+            Sleep2 = 0x1C,
+            Grounded = 0x20,
+            Grounded2 = 0x24,
+            Cape = 0x28,
+            Empty = 0x2C,
+            Disabled = 0x30,
+            ScrewAttack = 0x38,
+            PoisonFlower = 0x3C,
+            Nothing = 0x40
+        }
+        [CategoryAttribute("Stats")]
+        public int Damage
+        {
+            get { return *(bushort*)(data + 2) >> 0 & 0x1FF; }
+        }
+        [CategoryAttribute("Stats")]
+        public int KnockbackGrowth
+        {
+            get { return *(bushort*)(data + 13) >> 6 & 0x1FF; }
+        }
+        [CategoryAttribute("Stats")]
+        public int WeightDependantKnockback
+        {
+            get { return *(bushort*)(data + 14) >> 5 & 0x1FF; }
+        }
+        [CategoryAttribute("Stats")]
+        public ElementType Element
+        {
+            get { return (ElementType)(*(bushort*)(data + 17) >> 2 & 0x1F); }
+        }
+        [CategoryAttribute("Stats")]
+        public int Angle
+        {
+            get { return *(bushort*)(data + 12) >> 7 & 0xFFFF; }
+        }
+    }
+    public unsafe class ThrowCommand : ScriptCommand
+    {
+        public ThrowCommand(byte* dataptr)
+            : base(dataptr)
+        {
+        }
+        public enum ThrowTypes
+        {
+            Throw = 0x00,
+            Release = 0x01,
+        }
+        public ThrowTypes ThrowType
+        {
+            get { return (ThrowTypes)(*(bushort*)(data) >> 7 & 0x1); }
+        }
+    }
+    public unsafe class StartLoopCommand : ScriptCommand
+    {
+        public StartLoopCommand(byte* dataptr)
+            : base(dataptr)
+        {
+        }
+
+        public new string Name
+        {
+            get { return base.Name + "[" + Iterations + "]"; }
+        }
+        [CategoryAttribute("Loop Params")]
+        public ushort Iterations
+        {
+            get { return *(bushort*)(data + 2); }
+        }
+    }
+    public unsafe class PointerCommand : ScriptCommand
+    {
+
+        public PointerCommand(byte* dataptr)
+            : base(dataptr)
+        {
+        }
+
+        public new string Name
+        {
+            get { return base.Name + " @" + Pointer + ""; }
+        }
+        [CategoryAttribute("Pointer Params")]
+        public string Pointer
+        {
+            get
+            {
+                int pointer = (*(int*)(data + 4));
+                if (BitConverter.IsLittleEndian) pointer = pointer.Reverse();
+                return pointer.ToString("X8");
+            }
+            set(int val)
+            {
+                Pointer = Int32.Parse((*(int*)(data + 4)), System.Globalization.NumberStyles.AllowHexSpecifier)
+            }
+
         }
     }
 }
