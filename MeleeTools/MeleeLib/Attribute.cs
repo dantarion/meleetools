202a5595e27a11e47f82e96514ed4285d8924184
zephyr.owl <zephyr.owl@5eb10746-d8a1-70fc-444b-4eb12e85a3b0>
2010-07-26 18:10:39 +0000

    Removed dictionary from struct Attributes: A switch statement is more efficient and Dictionary offers no necessary additional functionality.

--- MeleeTools/MeleeLib/Attribute.cs
+++ MeleeTools/MeleeLib/Attribute.cs
@@ -8,60 +8,59 @@ namespace MeleeLib
 
     public struct Attribute
     {
-        static Dictionary<int, string> dict;
-        static String getName(int offset)
+        public string Name
         {
-            if (dict == null)
+            get
             {
-                dict = new Dictionary<int, string>();
-                dict[0x000] = "Walk Initial Velocity";
-                dict[0x004] = "Walk Acceleration?";
-                dict[0x008] = "Walk Maximum Velocity";
-                dict[0x00C] = "Walk Animation Speed";
-                dict[0x014] = "Same as 0xC?";
-                dict[0x018] = "Friction";
-                dict[0x01C] = "Dash Initial Velocity";
-                dict[0x020] = "StopTurn Initial Velocity";
-                dict[0x028] = "Run Initial Velocity?";
-                dict[0x030] = "Run Acceleration";
-                dict[0x038] = "Jump Startup Lag (Frames)";
-                dict[0x03C] = "same as 0x034?";
-                dict[0x040] = "Jump V Initial Velocity???";
-                dict[0x044] = "same as 0x03C";
-                dict[0x048] = "Jump H Initial Velocity???";
-                dict[0x04C] = "Hop V Initial Velocity???";
-                dict[0x050] = "Air Jump Multiplier";
-                dict[0x054] = "Same as 0x04C";
-                dict[0x058] = "Number of Jumps";
-                dict[0x05C] = "Gravity";
-                dict[0x060] = "Terminal Velocity";
-                dict[0x064] = "Aerial Mobility";
-                dict[0x068] = "Aerial Stopping Mobility";
-                dict[0x06C] = "Max Aerial H Velocity";
-                dict[0x070] = "same as 0x088?";
-                dict[0x074] = "Fast Fall Terminal Velocity";
-                dict[0x078] = "0024?";
-                dict[0x088] = "Weight";
-                dict[0x08C] = "Model Scaling";
-                dict[0x090] = "Shield Size";
-                dict[0x094] = "Shield Break Initial Velocity";
-                dict[0x0A8] = "Ledgejump Horizontal Velocity";
-                dict[0x0AC] = "Ledgejump Vertical Velocity";
-                dict[0x000] = "Normal Landing Lag";
-                dict[0x000] = "N-Air Landing Lag";
-                dict[0x0EC] = "F-Air Landing Lag";
-                dict[0x0F0] = "B-Air Landing Lag";
-                dict[0x0F4] = "D-Air Landing Lag";
-                dict[0x0F8] = "U-Air Landing Lag";
-                dict[0x160] = "Ice Traction?";
-                dict[0x17C] = "Special Jump Action = -1...?";
+                switch (_offset)
+                {
+                    case 0x000: return "Walk Initial Velocity";
+                    case 0x004: return "Walk Acceleration?";
+                    case 0x008: return "Walk Maximum Velocity";
+                    case 0x00C: return "Walk Animation Speed";
+                    case 0x014: return "Same as 0xC?";
+                    case 0x018: return "Friction";
+                    case 0x01C: return "Dash Initial Velocity";
+                    case 0x020: return "StopTurn Initial Velocity";
+                    case 0x028: return "Run Initial Velocity?";
+                    case 0x030: return "Run Acceleration";
+                    case 0x038: return "Jump Startup Lag (Frames)";
+                    case 0x03C: return "same as 0x034?";
+                    case 0x040: return "Jump V Initial Velocity???";
+                    case 0x044: return "same as 0x03C";
+                    case 0x048: return "Jump H Initial Velocity???";
+                    case 0x04C: return "Hop V Initial Velocity???";
+                    case 0x050: return "Air Jump Multiplier";
+                    case 0x054: return "Same as 0x04C";
+                    case 0x058: return "Number of Jumps";
+                    case 0x05C: return "Gravity";
+                    case 0x060: return "Terminal Velocity";
+                    case 0x064: return "Aerial Mobility";
+                    case 0x068: return "Aerial Stopping Mobility";
+                    case 0x06C: return "Max Aerial H Velocity";
+                    case 0x070: return "same as 0x088?";
+                    case 0x074: return "Fast Fall Terminal Velocity";
+                    case 0x078: return "0024?";
+                    case 0x088: return "Weight";
+                    case 0x08C: return "Model Scaling";
+                    case 0x090: return "Shield Size";
+                    case 0x094: return "Shield Break Initial Velocity";
+                    case 0x0A8: return "Ledgejump Horizontal Velocity";
+                    case 0x0AC: return "Ledgejump Vertical Velocity";
+                    case 0x0DC: return "Normal Landing Lag";
+                    case 0x0E0: return "N-Air Landing Lag";
+                    case 0x0EC: return "F-Air Landing Lag";
+                    case 0x0F0: return "B-Air Landing Lag";
+                    case 0x0F4: return "D-Air Landing Lag";
+                    case 0x0F8: return "U-Air Landing Lag";
+                    case 0x160: return "Ice Traction?";
+                    case 0x17C: return "Special Jump Action = -1...?";
+                }
+                return "";
             }
-            return dict.ContainsKey(offset) ? dict[offset] : "";
         }
-        public int Offset { get; set; }
-        public string Name { get { return getName(Offset); } }
+        private int _offset;
+        public int Offset { get { return _offset; } set { _offset = value; } }
         public object Value { get; set; }
-
-
     }
 }
