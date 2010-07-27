e2f355ea5838bdfc8ab438e438668f159da7a78a
zephyr.owl <zephyr.owl@5eb10746-d8a1-70fc-444b-4eb12e85a3b0>
2010-07-27 00:30:43 +0000

    Program is completely broken at the moment; please bare with me...

--- MeleeTools/MeleeLib/DatFile.cs
+++ MeleeTools/MeleeLib/DatFile.cs
@@ -3,56 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using MeleeLib.System;
+
 namespace MeleeLib
 {
     public class DatFile
     {
-        static int HEADER_LENGTH = 0x20;
-        public String Filename { get; set; }
+        public readonly String Filename;
+        public readonly ArraySlice<byte> RawFile;
+        public readonly ArraySlice<byte> RawData;
+        public readonly DatHeader Header;
+        public readonly Dictionary<string, SectionHeader> Section1Entries;
+        public readonly Dictionary<string, SectionHeader> Section2Entries;
+        public readonly FTHeader FTHeader;
+        public readonly List<Attribute> Attributes;
+        public readonly List<Subaction> Subactions;
 
-        public DatHeader Header { get { return header; } }
-        public Dictionary<String, SectionHeader> Section1Entries { get { return section1s; } }
-        public Dictionary<String, SectionHeader> Section2Entries { get { return section2s; } }
-        public FTHeader FTHeader { get { return ftheader; } }
-        public List<Attribute> Attributes { get { return attributes; } }
-        public List<Subaction> Subactions { get { return subactions; } }
-
-
-        private byte[] rawheader = new byte[HEADER_LENGTH];
-        private byte[] rawdata;
-        private DatHeader header;
-        private Dictionary<String, SectionHeader> section1s = new Dictionary<String, SectionHeader>();
-        private Dictionary<String, SectionHeader> section2s = new Dictionary<String, SectionHeader>();
-        private FTHeader ftheader;
-        private List<Attribute> attributes = new List<Attribute>();
-        private List<Subaction> subactions = new List<Subaction>();
-        public unsafe DatFile(string filename)
+        public DatFile(string filename)
         {
-
-            this.Filename = filename;
+            Section2Entries = new Dictionary<String, SectionHeader>();
+            Section1Entries = new Dictionary<String, SectionHeader>();
+            Attributes = new List<Attribute>();
+            Subactions = new List<Subaction>();
+            Filename = filename;
             //Load up file
-            var stream =  File.OpenRead(filename);
-
-            stream.Read(rawheader, 0,HEADER_LENGTH);
+            var stream = File.OpenRead(filename);
+            if (stream.Length > int.MaxValue) throw new IOException("File too large.");
+            RawFile = new byte[(int)stream.Length].Slice();
             //Get the header
-            fixed (byte* ptr = rawheader)
-            {
-                header = *(DatHeader*)ptr;
-            }
+            Header = new DatHeader(this);
             //Allocate space for the rest of the file
-            rawdata = new byte[header.Filesize - HEADER_LENGTH];
-            //Read rest of file
-            stream.Read(rawdata, 0, rawdata.Count());
+            RawData = RawFile.Slice(DatHeader.HeaderLength);
             //Compute offset base for the section name strings (They are near the end of the file)
-            uint stringoffsetbase = header.Datasize + header.OffsetCount * 4 + header.SectionType1Count * 8 + header.SectionType2Count * 8;
+            uint stringoffsetbase = Header.Datasize + Header.OffsetCount * 4 + Header.SectionType1Count * 8 + Header.SectionType2Count * 8;
             //Read SectionType1s
-            for (int i = 0; i < header.SectionType1Count; i++)
+            for (var i = 0; i < Header.SectionType1Count; i++)
             {
                 fixed (byte* ptr = rawdata)
                 {
                     SectionHeader section = *(SectionHeader*)(ptr + header.Datasize + header.OffsetCount * 4 + i * 8);
                     string name = new String((sbyte*)ptr + stringoffsetbase + section.StringOffset);
-                    section1s[name] = section;
+                    Section1Entries[name] = section;
                 }
             }
             //Read SectionType2s
@@ -63,7 +54,7 @@ namespace MeleeLib
                     SectionHeader section = *(SectionHeader*)(ptr + header.Datasize + header.OffsetCount * 4 + header.SectionType1Count * 8 + i * 8);
                     Console.WriteLine(section.StringOffset);
                     string name = new String((sbyte*)ptr + stringoffsetbase + section.StringOffset);
-                    section2s[name] = section;
+                    Section2Entries[name] = section;
 
                 }
             }
@@ -71,19 +62,19 @@ namespace MeleeLib
             fixed (byte* ptr = rawdata)
             {
                 int[] INT_ATTRIBUTES = { 0x58, 0xa4, 0x98, 0x16c };
-                ftheader = *(FTHeader*)(ptr + section1s.Values.First().DataOffset);
-                byte* cur = ftheader.AttributesOffset + ptr;
-                byte* end = ftheader.AttributesOffset2 + ptr;
+                FTHeader = *(FTHeader*)(ptr + Section1Entries.Values.First().DataOffset);
+                byte* cur = FTHeader.AttributesOffset + ptr;
+                byte* end = FTHeader.AttributesOffset2 + ptr;
                 int i = 0;
                 while (cur < end)
                 {
-                    Attribute attribute = new Attribute();
+                    var attribute = new Attribute();
                     if (!INT_ATTRIBUTES.Contains(i))
                         attribute.Value = (float)*(bfloat*)cur;
                     else
                         attribute.Value = (uint)*(buint*)cur;
                     attribute.Offset = i;
-                    attributes.Add(attribute);
+                    Attributes.Add(attribute);
                     i += 4;
                     cur += 4;
                 }
@@ -91,29 +82,29 @@ namespace MeleeLib
             //Subactions
             fixed (byte* ptr = rawdata)
             {
-                byte* cur = ftheader.SubactionStart + ptr;
-                byte* end = ftheader.SubactionEnd + ptr;
+                byte* cur = FTHeader.SubactionStart + ptr;
+                byte* end = FTHeader.SubactionEnd + ptr;
                 int i = 0;
                 while (cur < end)
                 {
                     Subaction s = new Subaction();
                     s.Header = *(SubactionHeader*)(cur);
                     string str = new String((sbyte*)ptr + s.Header.StringOffset);
-                    if(str.Contains("ACTION_"))
-                        str = str.Substring(str.LastIndexOf("ACTION_")+7).Replace("_figatree","");
+                    if (str.Contains("ACTION_"))
+                        str = str.Substring(str.LastIndexOf("ACTION_") + 7).Replace("_figatree", "");
                     if (str == "")
                         str = "[No name]";
                     s.Name = str;
                     s.Index = i;
                     s.Commands = readScript(ptr + s.Header.ScriptOffset);
-                    Subactions.Add(s);                    
+                    Subactions.Add(s);
                     i += 1;
-                    cur += 4*6;
+                    cur += 4 * 6;
                 }
             }
 
         }
-        public unsafe List<ScriptCommand> readScript(byte* ptr)
+        public List<ScriptCommand> readScript(byte* ptr)
         {
             var list = new List<ScriptCommand>();
             ScriptCommand sc = ScriptCommand.Factory(ptr);
